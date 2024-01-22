45372b5864e49f8341a2656fb3f38e9bbcc03fd0
sabitmemmedov <146194973+sabitmemmedov@users.noreply.github.com>
2024-01-22 21:32:01 +0400

    last commit

---
+++ simpleNavbar/assets/js/basket.js
@@ -0,0 +1,14 @@
+let navmobile = document.getElementById("navmobile")
+
+function navMobile() {
+    if (navmobile.style.display === 'block') {
+        navmobile.style.display = "none"
+    } else {
+        navmobile.style.display = "block"
+    }
+}
+
+
+
+
+
